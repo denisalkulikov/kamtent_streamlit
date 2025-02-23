d5b145cdd2fea114679162ceb53a51dda5d3daf9
denisalkulikov <denisalkulikov@gmail.com>
2025-02-24 01:53:23 +0400

    Добавлен расчёт тента сдвижной крыши + тента с рекламой + чекбоксы ЮЛ и предоставления скидки

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import math
 
 
 # Функция для первой страницы (калькулятор пологов)
@@ -132,33 +133,65 @@ def page_auto_calculator():
     # Словарь с ценами на материалы
     price = {
         'pvh_630': 250,
-        'pvh_650': 385,
-        'pvh_750': 450,
-        'pvh_900': 500,
-        'work': 550,
-        'babochka': 100
-    }
-
-    # Словарь с русскими названиями материалов
-    material_names = {
-        'pvh_630': 'ПВХ630',
-        'pvh_650': 'ПВХ650',
-        'pvh_750': 'ПВХ750',
-        'pvh_900': 'ПВХ900'
+         'pvh_650': 385,
+         'pvh_750': 450,
+         'pvh_900': 500,
+         'fanera_21_2': 5500,
+         'fanera_21_3': 9400,
+         'fanera_18': 4700,
+         'petlya_gaz': 270,
+         'petlya_kamaz': 530,
+         'zamki_gaz': 2500,
+         'zamki_kamaz': 2900,
+         'uplotnitel_18': 290,
+         'uplotnitel_21': 400,
+         'work': 550,
+         'truba_30_30_2': 874,
+         'truba_60_40_3': 1752,
+         'kronshtein': 212.4,
+         'shveler': 1075.3,
+         'podship_big': 60,
+         'podship_small': 60,
+         'amortizator': 1300,
+         'fiksator': 250,
+         'profil': 1300,
+         'plastina_650': 350,
+         'plastina_black': 70,
+         'gaika_m14': 6.57,
+         'bolt_10_20': 3.98,
+         'bolt_12_30': 7.3,
+         'du_15': 700,
+         'shnur_8': 70,
+         'remeshok': 80,
+         'tros': 150,
+         'shtorn_profil': 1200,
+         'babochka': 100,
+         'reklama': 750,
+         'truba_40_40_3': 1374,
+         'truba_40_40_2': 960,
+         'truba_40_20_2': 702,
+         'doska': 205,
+         'truba_60_40_2': 1200
     }
 
     # Ввод данных
     length = st.number_input("Введите длину (м)", value=1.0, min_value=0.01)
     width = st.number_input("Введите ширину (м)", value=1.0, min_value=0.01)
-    height_p = st.number_input("Введите полезную высоту (м)", value=1.0, min_value=0.01)
     height_g = st.number_input("Введите высоту готовой стенки (м)", value=1.0, min_value=0.01)
-    height_b = st.number_input("Введите высоту борта (м)", value=1.0, min_value=0.01)
 
-    # Чекбокс для ворот
-    is_vorota = st.checkbox("Наличие ворот")
+    # Чекбоксы для ворот и щита
+    col1, col2 = st.columns(2)  # Разделяем на две колонки
+    with col1:
+        is_vorota = st.checkbox("Наличие ворот")
+        is_schit = st.checkbox("Наличие щита")
+    with col2:
+        is_legal_entity = st.checkbox("Юридическое лицо")
+        apply_discount = st.checkbox("Предоставить скидку")
 
-    # Чекбокс для щита
-    is_schit = st.checkbox("Наличие щита")
+    # Поле для ввода скидки (появляется, если чекбокс выбран)
+    discount_percent = 0
+    if apply_discount:
+        discount_percent = st.number_input("Введите размер скидки (в %)", value=0, min_value=0, max_value=100)
 
     # Функция для расчёта площади изделия
     def calculate_area(length, width, height_g, is_vorota, is_schit):
@@ -171,8 +204,8 @@ def page_auto_calculator():
             sq = (length * height_g * 2) + (width * height_g * 2) + (length * width)
         return sq
 
-    # Функция для расчёта
-    def calculate_cost(material_price, area):
+    # Функция для расчёта стоимости ткани
+    def calculate_cost(material_price, area, is_legal_entity, discount_percent):
         # Стоимость ткани
         if length < 5:
             cost = material_price * 2.8
@@ -180,14 +213,23 @@ def page_auto_calculator():
             cost = material_price * 2.4
         else:
             cost = material_price * 2.6
-        # Округляем до сотых в большую сторону
         cost -= cost % -10
         cost = cost * area
+
+        # Увеличиваем стоимость на 25%, если выбрано юридическое лицо
+        if is_legal_entity:
+            cost *= 1.25
+
+        # Применяем скидку, если она указана
+        if discount_percent > 0:
+            cost *= (1 - discount_percent / 100)
+
+        # Округляем до сотых в большую сторону
         cost -= cost % -100
         return cost
 
     # Функция для расчёта стоимости "Тент (чулок)"
-    def calculate_chulok_cost(material_price, area, length):
+    def calculate_chulok_cost(material_price, area, length, is_legal_entity, discount_percent):
         # Стоимость ткани
         fabric_cost = material_price * area
         fabric_cost -= fabric_cost % -100
@@ -204,6 +246,85 @@ def page_auto_calculator():
         # Итоговая стоимость
         total_cost = (fabric_cost + babochka_cost + work_cost) * 2
 
+        # Увеличиваем стоимость на 25%, если выбрано юридическое лицо
+        if is_legal_entity:
+            total_cost *= 1.25
+
+        # Применяем скидку, если она указана
+        if discount_percent > 0:
+            total_cost *= (1 - discount_percent / 100)
+
+        # Округляем до сотых в большую сторону
+        total_cost -= total_cost % -100
+        return total_cost
+
+    # Функция для расчёта стоимости "Тент сдвижной крыши"
+    def calculate_sdvizhnoy_krysha_cost(material_price, length, width, is_legal_entity, discount_percent):
+        # Площадь крыши
+        s_krysha = (length + 0.6) * (width + 0.6)
+
+        # Длина шнура
+        p_shnur = length * 2 + 2
+
+        # Количество пластин 650
+        if math.ceil((length - 1) / 0.65 * 2) % 2 == 0:
+            count_plastin_650 = math.ceil((length - 1) / 0.65 * 2)
+        else:
+            count_plastin_650 = math.ceil((length - 1) / 0.65 * 2) + 1
+
+        # Количество ремешков
+        count_remeshki = ((round(count_plastin_650 / 2 + 1)) - 4) * 3 + 20
+
+        # Длина усилителя
+        p_usilitel = (round(count_plastin_650 / 2 + 1) * width * 0.1) + ((length * 2 + width * 2) * 0.15)
+
+        # Количество работы
+        count_work = length * 1.5
+
+        # Итоговая стоимость
+        total_cost = (
+            s_krysha * material_price +  # Стоимость ткани
+            p_shnur * price['shnur_8'] +   # Стоимость шнура
+            count_remeshki * price['remeshok'] +  # Стоимость ремешков
+            p_usilitel * material_price +  # Стоимость усилителя
+            count_work * price['work']  # Стоимость работы
+        )
+
+        # Увеличиваем стоимость на 25%, если выбрано юридическое лицо
+        if is_legal_entity:
+            total_cost *= 1.25
+
+        # Применяем скидку, если она указана
+        if discount_percent > 0:
+            total_cost *= (1 - discount_percent / 100)
+
+        # Округляем до сотых в большую сторону
+        total_cost = total_cost * 1.7
+        total_cost -= total_cost % -100
+        return total_cost
+
+    # Функция для расчёта стоимости тента с рекламой
+    def calculate_reklama_cost(reklama_area, total_area, material_price, length, is_legal_entity, discount_percent):
+        # Стоимость рекламы
+        reklama_cost = reklama_area * price['reklama']
+
+        # Стоимость ткани
+        fabric_cost = (total_area - reklama_area) * material_price
+
+        # Стоимость работы
+        work_cost = (length / 0.27) * price['work']
+
+        # Итоговая стоимость
+        total_cost = (reklama_cost + fabric_cost + work_cost) * 2
+
+        # Увеличиваем стоимость на 25%, если выбрано юридическое лицо
+        if is_legal_entity:
+            total_cost *= 1.25
+
+        # Применяем скидку, если она указана
+        if discount_percent > 0:
+            total_cost *= (1 - discount_percent / 100)
+
         # Округляем до сотых в большую сторону
         total_cost -= total_cost % -100
         return total_cost
@@ -211,41 +332,126 @@ def page_auto_calculator():
     # Рассчитываем площадь
     area = calculate_area(length, width, height_g, is_vorota, is_schit)
 
+    # Площади для рекламы
+    reklama_2_stenki = length * height_g * 2
+    reklama_2_stenki_and_klapan = (length * height_g * 2) + (width * height_g)
+    reklama_2_stenki_and_krysha = (length * height_g * 2) + (length * width)
+    reklama_2_stenki_and_klapan_and_krysha = (length * height_g * 2) + (width * height_g) + (length * width)
+
     # Создаём список для хранения результатов
     results_typical = []  # Для типового тента
     results_chulok = []   # Для тента (чулок)
+    results_sdvizhnoy = []  # Для тента сдвижной крыши
+    results_reklama_2_stenki = []  # Для тента с рекламой (2 стенки)
+    results_reklama_2_stenki_and_klapan = []  # Для тента с рекламой (2 стенки + клапан)
+    results_reklama_2_stenki_and_krysha = []  # Для тента с рекламой (2 стенки + крыша)
+    results_reklama_2_stenki_and_klapan_and_krysha = []  # Для тента с рекламой (2 стенки + клапан + крыша)
 
     # Расчёт для каждого материала
     for material, material_price in price.items():
-        if material not in ['babochka', 'work']:  # Исключаем бабочки и работу
+        if material not in ['babochka', 'work', 'shnur_8', 'remeshok', 'reklama']:  # Исключаем бабочки, работу, шнур, ремешки и рекламу
             # Стоимость для типового тента
-            cost_typical = calculate_cost(material_price, area)
+            cost_typical = calculate_cost(material_price, area, is_legal_entity, discount_percent)
             results_typical.append(cost_typical)
 
             # Стоимость для тента (чулок)
-            cost_chulok = calculate_chulok_cost(material_price, area, length)
+            cost_chulok = calculate_chulok_cost(material_price, area, length, is_legal_entity, discount_percent)
             results_chulok.append(cost_chulok)
 
-    # Создаём DataFrame для таблицы
+            # Стоимость для тента сдвижной крыши
+            cost_sdvizhnoy = calculate_sdvizhnoy_krysha_cost(material_price, length, width, is_legal_entity, discount_percent)
+            results_sdvizhnoy.append(cost_sdvizhnoy)
+
+            # Стоимость для тента с рекламой (2 стенки)
+            cost_reklama_2_stenki = calculate_reklama_cost(reklama_2_stenki, area, material_price, length, is_legal_entity, discount_percent)
+            results_reklama_2_stenki.append(cost_reklama_2_stenki)
+
+            # Стоимость для тента с рекламой (2 стенки + клапан)
+            if not is_vorota:
+                cost_reklama_2_stenki_and_klapan = calculate_reklama_cost(reklama_2_stenki_and_klapan, area, material_price, length, is_legal_entity, discount_percent)
+                results_reklama_2_stenki_and_klapan.append(cost_reklama_2_stenki_and_klapan)
+            else:
+                results_reklama_2_stenki_and_klapan.append("НЕВЕРНО!")
+
+            # Стоимость для тента с рекламой (2 стенки + крыша)
+            cost_reklama_2_stenki_and_krysha = calculate_reklama_cost(reklama_2_stenki_and_krysha, area, material_price, length, is_legal_entity, discount_percent)
+            results_reklama_2_stenki_and_krysha.append(cost_reklama_2_stenki_and_krysha)
+
+            # Стоимость для тента с рекламой (2 стенки + клапан + крыша)
+            if not is_vorota:
+                cost_reklama_2_stenki_and_klapan_and_krysha = calculate_reklama_cost(reklama_2_stenki_and_klapan_and_krysha, area, material_price, length, is_legal_entity, discount_percent)
+                results_reklama_2_stenki_and_klapan_and_krysha.append(cost_reklama_2_stenki_and_klapan_and_krysha)
+            else:
+                results_reklama_2_stenki_and_klapan_and_krysha.append("НЕВЕРНО!")
+
+    # Создаём DataFrame для основной таблицы
     results_df = pd.DataFrame({
-        "Тент": ["Тент типовой", "Тент (чулок)"],
-        "ПВХ630": [results_typical[0], results_chulok[0]],  # Для ПВХ630
-        "ПВХ650": [results_typical[1], results_chulok[1]],  # Для ПВХ650
-        "ПВХ750": [results_typical[2], results_chulok[2]],  # Для ПВХ750
-        "ПВХ900": [results_typical[3], results_chulok[3]],  # Для ПВХ900
+        "Тент": [
+            "Тент типовой",
+            "Тент сдвижной крыши",
+            "Тент (чулок)",
+        ],
+        "ПВХ630": [
+            results_typical[0],
+            results_sdvizhnoy[0],
+            results_chulok[0],
+        ],
+        "ПВХ650": [
+            results_typical[1],
+            results_sdvizhnoy[1],
+            results_chulok[1],
+        ],
+        "ПВХ750": [
+            results_typical[2],
+            results_sdvizhnoy[2],
+            results_chulok[2],
+        ],
+        "ПВХ900": [
+            results_typical[3],
+            results_sdvizhnoy[3],
+            results_chulok[3],
+        ],
+    })
+
+    # Создаём DataFrame для таблицы с рекламой
+    reklama_df = pd.DataFrame({
+        "Тент с рекламой": [
+            "Реклама на 2-х стенках",
+            "Реклама на 2-х стенках и клапане",
+            "Реклама на 2-х стенках и крыше",
+            "Реклама на 2-х стенках, клапане и крыше"
+        ],
+        "ПВХ650": [
+            results_reklama_2_stenki[1],
+            results_reklama_2_stenki_and_klapan[1],
+            results_reklama_2_stenki_and_krysha[1],
+            results_reklama_2_stenki_and_klapan_and_krysha[1]
+        ],
     })
 
     # Функция для форматирования стоимости с разделением на разряды
     def format_cost(cost):
+        if cost == "НЕВЕРНО!":
+            return cost
         return "{:,.2f} руб".format(cost).replace(",", " ")
 
     # Применяем форматирование к столбцам с ценами
     for col in ["ПВХ630", "ПВХ650", "ПВХ750", "ПВХ900"]:
         results_df[col] = results_df[col].apply(format_cost)
 
-    # Выводим таблицу с помощью st.dataframe
+    for col in ["ПВХ650"]:
+        reklama_df[col] = reklama_df[col].apply(format_cost)
+
+    # Выводим основную таблицу
     st.dataframe(
         results_df,
+        hide_index=True,  # Скрываем индексы
+        use_container_width=True  # Растягиваем таблицу на всю ширину контейнера
+    )
+
+    # Выводим таблицу с рекламой
+    st.dataframe(
+        reklama_df,
         hide_index=True,  # Скрываем индексы
         use_container_width=True  # Растягиваем таблицу на всю ширину контейнера
     )
